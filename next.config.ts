439e33c853d0a18f6b4674769b69911dc6132f0f
AubakirovArman <armanibadboy@mail.ru>
2025-06-06 13:12:35 +0500

    1
    1

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,27 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    optimizePackageImports: ['@xyflow/react']
+  },
+  // Оптимизация для шрифтов Google Fonts
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-DNS-Prefetch-Control',
+            value: 'on'
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY'
+          }
+        ]
+      }
+    ];
+  }
 };
 
 export default nextConfig;
